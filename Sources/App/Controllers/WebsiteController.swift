638a74dd020e9ab540c956f83ea5142bafaf0d75
Tommi Kivimäki <tommi@tommikivimaki.com>
2018-11-21 21:01:28 +0200

    Navigation and sub-pages

--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -6,6 +6,8 @@ struct WebsiteController: RouteCollection {
     
     router.get(use: indexHandler)
     router.get("comments", UserComment.parameter, use: userCommentHandler)
+    router.get("users", User.parameter, use: userHandler)
+    router.get("allUsers", use: allUsersHandler)
   }
   
   func indexHandler(_ req: Request) throws -> Future<View> {
@@ -13,21 +15,44 @@ struct WebsiteController: RouteCollection {
       .all()
       .flatMap(to: View.self) { userComments in
         let userCommentsData = userComments.isEmpty ? nil : userComments
-        let context = IndexContent(title: "User Comments", userComments: userCommentsData)
+        let context = IndexContent(title: "Comment Box", userComments: userCommentsData)
         return try req.view().render("index", context)
     }
   }
-
+  
   func userCommentHandler(_ req: Request) throws -> Future<View> {
     return try req.parameters.next(UserComment.self)
       .flatMap(to: View.self) { userComment in
         return userComment.user
-        .get(on: req)
+          .get(on: req)
           .flatMap(to: View.self) { user in
             let userCommentContext = UserCommentContext(timestamp: userComment.timestamp,
                                                         userComment: userComment, user: user)
             return try req.view().render("comments", userCommentContext)
         }
+    }
+  }
+  
+  func userHandler(_ req: Request) throws -> Future<View> {
+    return try req.parameters.next(User.self)
+      .flatMap(to: View.self) { user in
+        return try user.userComments
+          .query(on: req)
+          .all()
+          .flatMap(to: View.self) { userComments in
+            let userContext = UserContext(title: "User", user: user, userComments: userComments)
+            return try req.view().render("users", userContext)
+        }
+        
+    }
+  }
+  
+  func allUsersHandler(_ req: Request) throws -> Future<View> {
+    return User.query(on: req)
+      .all()
+      .flatMap(to: View.self) { users in
+        let allUsersContext = AllUsersContext(title: "All Comment Box Users", users: users)
+        return try req.view().render("allUsers", allUsersContext)
     }
   }
 }
@@ -43,4 +68,17 @@ struct UserCommentContext: Encodable {
   let timestamp: String
   let userComment: UserComment
   let user: User
+}
+
+// Data for User view
+struct UserContext: Encodable {
+  let title: String
+  let user: User
+  let userComments: [UserComment]
+}
+
+// Data for all users view
+struct AllUsersContext: Encodable {
+  let title: String
+  let users: [User]
 }
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -22,7 +22,7 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
   )
   let corsMiddleware = CORSMiddleware(configuration: corsConfiguration)
   middlewares.use(corsMiddleware)
-  // middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
+  middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
   middlewares.use(ErrorMiddleware.self)
   services.register(middlewares)
 
